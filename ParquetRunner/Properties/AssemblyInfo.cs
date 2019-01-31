282af0bb49b1146f16463db414eb00316d298499
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-01-31 13:40:15 -0800

    Renames the ParquetCLTester project to ParquetRunner.

--- ParquetRunner/Properties/AssemblyInfo.cs
+++ ParquetRunner/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("ParquetCLTester")]
+[assembly: AssemblyTitle("ParquetRunner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ParquetClassLibrary")]
