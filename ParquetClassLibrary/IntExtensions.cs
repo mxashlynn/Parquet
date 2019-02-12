647bf67094d5c87f9bde08ea137fc22898b77b7e
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-02-11 16:35:34 -0800

    Implements extension method to easily constrain the range of integer types.

---
+++ ParquetClassLibrary/IntExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace ParquetClassLibrary
+{
+    internal static class IntExtensions
+    {
+        /// <summary>Ensures an integer falls within the given range.</summary>
+        /// <param name="in_int">In int to normalize.</param>
+        /// <param name="in_lowBound">The lowest valid value for the int.</param>
+        /// <param name="in_upBound">The highest valid value for the int.</param>
+        /// <returns>The int, normalized.</returns>
+        public static int Normalize(this int in_int, int in_lowBound, int in_upBound)
+        {
+            if (in_int < in_lowBound)
+            {
+                in_int = in_lowBound;
+            }
+            else if (in_int > in_upBound)
+            {
+                in_int = in_upBound;
+            }
+
+            return in_int;
+        }
+    }
+}
