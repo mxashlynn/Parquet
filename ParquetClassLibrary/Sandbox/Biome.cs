102ddd9ac97ffb1a93c195863123ab145743c654
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-04-02 20:44:52 -0700

    Implements means to determine what is needed to access a Biome. Also adds a star rating for each Biome's difficulty tier.

--- ParquetClassLibrary/Sandbox/Biome.cs
+++ ParquetClassLibrary/Sandbox/Biome.cs
@@ -12,10 +12,149 @@
         Forest,
         Heavens,
         Inferno,
+        Ruins,
         Seaside,
         Swamp,
         Town,
         Tundra,
         Volcano,
+    }
+
+    /// <summary>
+    /// Convenience extension methods for <see cref="Biome"/> instances.
+    /// </summary>
+    internal static class BiomeExtensions
+    {
+        /// <summary>
+        /// Gets the tier-rating of the given biome in terms of number of stars.
+        /// </summary>
+        /// <param name="in_enumVariable">The Biome under consideration.</param>
+        /// <returns>The number of stars for this Biome's tier.</returns>
+        public static int GetStarRating(this ref Biome in_enumVariable)
+        {
+            var stars = 0;
+
+            switch (in_enumVariable)
+            {
+                case Biome.Field:
+                case Biome.Town:
+                    stars = 0;
+                    break;
+                case Biome.Forest:
+                case Biome.Seaside:
+                    stars = 1;
+                    break;
+                case Biome.Desert:
+                case Biome.Swamp:
+                case Biome.Tundra:
+                    stars = 2;
+                    break;
+                case Biome.Cavern:
+                    stars = 3;
+                    break;
+                case Biome.Alpine:
+                case Biome.Ruins:
+                case Biome.Volcano:
+                    stars = 4;
+                    break;
+                case Biome.Heavens:
+                case Biome.Inferno:
+                    stars = 5;
+                    break;
+            }
+
+            return stars;
+        }
+
+        /// <summary>
+        /// Gets an archtypical <see cref="Elevation"/> for the given biome.
+        /// </summary>
+        /// <param name="in_enumVariable">The Biome under consideration.</param>
+        /// <returns>The elevation of this Biome.</returns>
+        public static Elevation GetElevation(this ref Biome in_enumVariable)
+        {
+            var elevation = Elevation.LevelGround;
+
+            switch (in_enumVariable)
+            {
+                case Biome.Field:
+                case Biome.Town:
+                case Biome.Forest:
+                case Biome.Seaside:
+                case Biome.Desert:
+                case Biome.Swamp:
+                case Biome.Tundra:
+                case Biome.Ruins:
+                    elevation = Elevation.LevelGround;
+                    break;
+                case Biome.Cavern:
+                case Biome.Inferno:
+                    elevation = Elevation.BelowGround;
+                    break;
+                case Biome.Alpine:
+                case Biome.Volcano:
+                case Biome.Heavens:
+                    elevation = Elevation.AboveGround;
+                    break;
+            }
+
+            return elevation;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="BiomeKey"/> a player character needs to safely access the given biome.
+        /// </summary>
+        /// <param name="in_enumVariable">The Biome under consideration.</param>
+        /// <returns>The requirements needed to enter this Biome.</returns>
+        public static BiomeKey GetEntryRequirements(this ref Biome in_enumVariable)
+        {
+            var key = BiomeKey.None;
+
+            switch (in_enumVariable)
+            {
+                case Biome.Field:
+                case Biome.Town:
+                    key = BiomeKey.None;
+                    break;
+                case Biome.Forest:
+                    key = BiomeKey.Knapsack;
+                    break;
+                case Biome.Seaside:
+                    key = BiomeKey.Sunscreen;
+                    break;
+                case Biome.Desert:
+                    key = BiomeKey.Sunscreen | BiomeKey.Canteen | BiomeKey.Boots;
+                    break;
+                case Biome.Swamp:
+                    key = BiomeKey.Knapsack | BiomeKey.BugSpray | BiomeKey.Boots;
+                    break;
+                case Biome.Tundra:
+                    key = BiomeKey.Sunscreen | BiomeKey.WarmGear | BiomeKey.Boots;
+                    break;
+                case Biome.Cavern:
+                    key = BiomeKey.Knapsack | BiomeKey.Lantern | BiomeKey.Boots | BiomeKey.Rope;
+                    break;
+                case Biome.Alpine:
+                    key = BiomeKey.Knapsack | BiomeKey.Lantern | BiomeKey.Boots | BiomeKey.Rope
+                        | BiomeKey.Sunscreen | BiomeKey.WarmGear | BiomeKey.ClimbingKit;
+                    break;
+                case Biome.Ruins:
+                    key = BiomeKey.Knapsack | BiomeKey.Lantern | BiomeKey.Boots | BiomeKey.Rope
+                        | BiomeKey.Knapsack | BiomeKey.BugSpray | BiomeKey.Charm;
+                    break;
+                case Biome.Volcano:
+                    key = BiomeKey.Knapsack | BiomeKey.Lantern | BiomeKey.Boots | BiomeKey.Rope
+                        | BiomeKey.Sunscreen | BiomeKey.Canteen | BiomeKey.GasMask;
+                    break;
+                case Biome.Heavens:
+                    key = BiomeKey.PearlyFeather;
+                    break;
+                case Biome.Inferno:
+                    key = BiomeKey.CharredMask;
+                    break;
+            }
+
+            return key;
+        }
     }
 }
---
+++ ParquetClassLibrary/Sandbox/BiomeKey.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ParquetClassLibrary.Sandbox
+{
+    /// <summary>
+    /// Indicates the requirements for a player character to safely enter a <see cref="Biome"/>.
+    /// </summary>
+    [Flags]
+    public enum BiomeKey
+    {
+        None = 0,
+        Knapsack,
+        Sunscreen,
+        Boots,
+        Canteen,
+        BugSpray,
+        WarmGear,
+        Lantern,
+        Rope,
+        ClimbingKit,
+        GasMask,
+        Charm,
+        PearlyFeather,
+        CharredMask,
+    }
+
+    /// <summary>
+    /// Convenience extension methods for concise coding when working with <see cref="BiomeKey"/> instances.
+    /// </summary>
+    internal static class BiomeKeyExtensions
+    {
+        /// <summary>
+        /// Sets the given flag in the specified variable.
+        /// </summary>
+        /// <param name="in_enumVariable">The enum variable under consideration.</param>
+        /// <param name="in_flagToTest">The flag to set.</param>
+        /// <returns>The variable with the flag set.</returns>
+        public static BiomeKey Set(this ref BiomeKey in_enumVariable, BiomeKey in_flagToTest)
+        {
+            return in_enumVariable = in_enumVariable | in_flagToTest;
+        }
+
+        /// <summary>
+        /// Clears the given flag in the specified variable.
+        /// </summary>
+        /// <param name="in_enumVariable">The enum variable under consideration.</param>
+        /// <param name="in_flagToTest">The flag to clear.</param>
+        /// <returns>The variable with the flag cleared.</returns>
+        public static BiomeKey Clear(this ref BiomeKey in_enumVariable, BiomeKey in_flagToTest)
+        {
+            return in_enumVariable = in_enumVariable & ~in_flagToTest;
+        }
+
+        /// <summary>
+        /// Checks if the given flag is set.
+        /// </summary>
+        /// <param name="in_enumVariable">The enum variable under consideration.</param>
+        /// <param name="in_flagToTest">The flag to test.</param>
+        /// <returns><c>true</c>, if at least this flag is set, <c>false</c> otherwise.</returns>
+        public static bool IsSet(this BiomeKey in_enumVariable, BiomeKey in_flagToTest)
+        {
+            return (in_enumVariable & in_flagToTest) == in_flagToTest;
+        }
+
+        /// <summary>
+        /// Checks if the given flag is unset.
+        /// </summary>
+        /// <param name="in_enumVariable">The enum variable under consideration.</param>
+        /// <param name="in_flagToTest">The flag to test.</param>
+        /// <returns><c>true</c>, if at least this flag is unset, <c>false</c> otherwise.</returns>
+        public static bool IsNotSet(this BiomeKey in_enumVariable, BiomeKey in_flagToTest)
+        {
+            return (in_enumVariable & ~in_flagToTest) == BiomeKey.None;
+        }
+
+        /// <summary>
+        /// Sets or clears the given flag in the specified variable, depending on the given boolean.
+        /// </summary>
+        /// <param name="in_enumVariable">The enum variable under consideration.</param>
+        /// <param name="in_flagToTest">The flag to set or clear.</param>
+        /// <param name="in_state">If <c>true</c>, the flag will be set; otherwise it will be cleared.</param>
+        /// <returns>The variable with the flag modified.</returns>
+        public static BiomeKey SetTo(this ref BiomeKey in_enumVariable, BiomeKey in_flagToTest, bool in_state)
+        {
+            return in_enumVariable = in_state
+                ? in_enumVariable.Set(in_flagToTest)
+                : in_enumVariable.Clear(in_flagToTest);
+        }
+    }
+}
