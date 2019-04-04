0f486f7ddc54a0c5cdbf97758ac843ffa2673b67
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-04-04 16:04:25 -0700

    Various additions and fixes to bounds checking in CraftingRecipe.

--- ParquetClassLibrary/Crafting/CraftingRecipe.cs
+++ ParquetClassLibrary/Crafting/CraftingRecipe.cs
@@ -28,9 +28,19 @@ namespace ParquetClassLibrary.Crafting
         /// <param name="in_quantityProduced">The number of items created by following this recipe.</param>
         /// <param name="in_ingredients">All items needed to follow this recipe once.</param>
         /// <param name="in_panelPattern">The arrangment of panels encompassed by this recipe.</param>
+        /// <exception cref="ArgumentNullException">Thrown when in_ingredients is null.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when in_panelPattern is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when in_quantityProduced is less than 1.</exception>
         /// <exception cref="ArgumentOutOfRangeException">
-        /// Thrown when the either the in_itemProduced or any of the in_ingredients are not within
-        /// the <c see="AssemblyInfo.ItemIDs"/> range.
+        /// Thrown when in_itemProduced is not within the <c see="AssemblyInfo.ItemIDs"/> range.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when any in_ingredients are not within the <c see="AssemblyInfo.ItemIDs"/> range.
+        /// </exception>
+        /// <exception cref="IndexOutOfRangeException">Thrown when in_ingredients is empty.</exception>
+        /// <exception cref="IndexOutOfRangeException">
+        /// Thrown when in_panelPattern has dimensions other than those given by
+        /// <see cref="AssemblyInfo.PanelPatternWidth"/> and <see cref="AssemblyInfo.PanelPatternHeight"/>.
         /// </exception>
         public CraftingRecipe(EntityID in_itemProduced, int in_quantityProduced,
                               List<EntityID> in_ingredients, StrikePanel[,] in_panelPattern)
@@ -39,12 +49,24 @@ namespace ParquetClassLibrary.Crafting
             {
                 throw new ArgumentOutOfRangeException(nameof(in_itemProduced));
             }
+            if (in_quantityProduced < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(in_quantityProduced));
+            }
             foreach (var ingredient in in_ingredients)
             {
                 if (!ingredient.IsValidForRange(AssemblyInfo.ItemIDs))
                 {
                     throw new ArgumentOutOfRangeException(nameof(in_ingredients));
                 }
+            }
+            if (null == in_ingredients)
+            {
+                throw new ArgumentNullException(nameof(in_ingredients));
+            }
+            if (in_ingredients.Count < 1)
+            {
+                throw new IndexOutOfRangeException(nameof(in_ingredients));
             }
             if (in_panelPattern.GetLength(0) != AssemblyInfo.PanelPatternWidth
                 || in_panelPattern.GetLength(1) != AssemblyInfo.PanelPatternHeight)
@@ -55,7 +77,7 @@ namespace ParquetClassLibrary.Crafting
             ItemProduced = in_itemProduced;
             QuantityProduced = in_quantityProduced;
             Ingredients = in_ingredients;
-            PanelPattern = in_panelPattern;
+            PanelPattern = in_panelPattern ?? throw new ArgumentNullException(nameof(in_panelPattern));
         }
     }
 }
