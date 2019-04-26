a74e46018f1da44b873fe63b28e28a3e5856c1e6
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-04-26 14:49:20 -0700

    Implements the Space container class.

---
+++ ParquetClassLibrary/Sandbox/Space.cs
@@ -0,0 +1,17 @@
+﻿using ParquetClassLibrary.Sandbox.Parquets;
+using ParquetClassLibrary.Stubs;
+
+namespace ParquetClassLibrary.Sandbox
+{
+    /// <summary>
+    /// A <see cref="ParquetStack"/> together with its coordinates on the world map.
+    /// </summary>
+    public class Space
+    {
+        /// <summary>Location of this <see cref="Space"/>.</summary>
+        public readonly Vector2Int Position;
+
+        /// <summary>All parquets occupying this <see cref="Space"/>.</summary>
+        public readonly ParquetStack Content;
+    }
+}
