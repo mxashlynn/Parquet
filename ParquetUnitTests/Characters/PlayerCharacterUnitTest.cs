ac601154137da81f9fb51e8f8f25da64376d952d
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-05-04 20:50:14 -0700

    Implements PlayerCharacter unit tests.

---
+++ ParquetUnitTests/Characters/PlayerCharacterUnitTest.cs
@@ -0,0 +1,36 @@
+using System;
+using ParquetClassLibrary.Sandbox.IDs;
+using ParquetUnitTests;
+using Xunit;
+
+namespace ParquetClassLibrary.Characters
+{
+    public class PlayerCharacterUnitTest
+    {
+        #region Test Values
+        /// <summary>Identifier used when creating a new block.</summary>
+        private static readonly EntityID newPlayerID = TestEntities.TestPlayer.ID - 1;
+        #endregion
+
+        [Fact]
+        public void ValidPlayerIDsArePermittedTest()
+        {
+            var newPlayer = new PlayerCharacter(newPlayerID, "player character", "will be created");
+
+            Assert.NotNull(newPlayer);
+        }
+
+        [Fact]
+        public void InvalidNpcIDsRaiseExceptionTest()
+        {
+            var badNpcID = TestEntities.TestBlock.ID - 1;
+
+            void TestCode()
+            {
+                var _ = new PlayerCharacter(badNpcID, "player character", "will fail");
+            }
+
+            Assert.Throws<ArgumentOutOfRangeException>(TestCode);
+        }
+    }
+}
