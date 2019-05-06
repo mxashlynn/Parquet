9debb3d99f051843d484ead1d92cf4d4d5053af3
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-05-05 17:07:33 -0700

    Fixes some errors in CSV import tool.

--- ParquetCSVImporter/ImportProgram.cs
+++ ParquetCSVImporter/ImportProgram.cs
@@ -19,7 +19,7 @@ namespace ParquetCSVImporter
         /// <summary>The location of the Designer files.</summary>
         public static readonly string SearchPath =
 #if DEBUG
-            Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            $"{Directory.GetCurrentDirectory()}/../../../../";
 #else
             Directory.GetCurrentDirectory().FullName;
 #endif
