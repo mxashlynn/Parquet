62bcfe5af8ffdc7505e0e8861427c9a71c8f33b5
Paige Ashlynn <mx.ashlynn@gmail.com>
2019-06-14 17:47:18 -0700

    Makes Space IEquatable.

--- ParquetClassLibrary/Rooms/Space.cs
+++ ParquetClassLibrary/Rooms/Space.cs
@@ -1,4 +1,5 @@
-﻿using ParquetClassLibrary.Parquets;
+﻿using System;
+using ParquetClassLibrary.Parquets;
 #if UNITY_2018_4_OR_NEWER
 using UnityEngine;
 #else
@@ -8,12 +9,12 @@ using ParquetClassLibrary.Stubs;
 namespace ParquetClassLibrary.Rooms
 {
     /// <summary>
-    /// A <see cref="ParquetStack"/> together with its coordinates on the world map.
+    /// A <see cref="ParquetStack"/> together with its coordinates within a given <see cref="Map.MapRegion"/>.
     /// </summary>
-    public struct Space
+    public struct Space : IEquatable<Space>
     {
-        /// <summary>The null <see cref="Space"/>, representing an arbitrary empty <see cref="ParquetStack"/>.</summary>
-        public static readonly Space Empty = new Space(Vector2Int.ZeroVector, ParquetStack.Empty);
+        /// <summary>The null <see cref="Space"/>, which exists nowhere and contains nothing.</summary>
+        public static readonly Space Empty = new Space(new Vector2Int(int.MinValue, int.MinValue), ParquetStack.Empty);
 
         /// <summary>Location of this <see cref="Space"/>.</summary>
         public readonly Vector2Int Position;
@@ -21,6 +22,7 @@ namespace ParquetClassLibrary.Rooms
         /// <summary>All parquets occupying this <see cref="Space"/>.</summary>
         public readonly ParquetStack Content;
 
+        #region Initialization
         /// <summary>
         /// Initializes a new instance of the <see cref="Space"/> class.
         /// </summary>
@@ -43,5 +45,55 @@ namespace ParquetClassLibrary.Rooms
             Position = new Vector2Int(in_x, in_y);
             Content = in_content;
         }
+        #endregion
+
+        #region IEquatable Implementation
+        /// <summary>
+        /// Serves as a hash function for a <see cref="Space"/> struct.
+        /// </summary>
+        /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures.</returns>
+        public override int GetHashCode()
+            => (Position, Content).GetHashCode();
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Space"/> is equal to the current <see cref="Space"/>.
+        /// </summary>
+        /// <param name="in_space">The <see cref="Space"/> to compare with the current.</param>
+        /// <returns><c>true</c> if the <see cref="Space"/>s are equal.</returns>
+        public bool Equals(Space in_space)
+            => Position == in_space.Position
+            && Content == in_space.Content;
+
+        /// <summary>
+        /// Determines whether the specified <see cref="object"/> is equal to the current <see cref="Space"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="object"/> to compare with the current <see cref="Space"/>.</param>
+        /// <returns><c>true</c> if the specified <see cref="object"/> is equal to the current <see cref="Space"/>; otherwise, <c>false</c>.</returns>
+        // ReSharper disable once InconsistentNaming
+        public override bool Equals(object obj)
+            => obj is Space vector && Equals(vector);
+
+        /// <summary>
+        /// Determines whether a specified instance of <see cref="Space"/> is equal to
+        /// another specified instance of <see cref="Space"/>.
+        /// </summary>
+        /// <param name="in_space1">The first <see cref="Space"/> to compare.</param>
+        /// <param name="in_space2">The second <see cref="Space"/> to compare.</param>
+        /// <returns><c>true</c> if the two operands are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(Space in_space1, Space in_space2)
+            => in_space1.Position == in_space2.Position
+            && in_space1.Content == in_space2.Content;
+
+        /// <summary>
+        /// Determines whether a specified instance of <see cref="Space"/> is unequal to
+        /// another specified instance of <see cref="Space"/>.
+        /// </summary>
+        /// <param name="in_space1">The first <see cref="Space"/> to compare.</param>
+        /// <param name="in_space2">The second <see cref="Space"/> to compare.</param>
+        /// <returns><c>true</c> if the two operands are NOT equal; otherwise, <c>false</c>.</returns>
+        public static bool operator !=(Space in_space1, Space in_space2)
+            => in_space1.Position != in_space2.Position
+            && in_space1.Content != in_space2.Content;
+        #endregion
     }
 }
