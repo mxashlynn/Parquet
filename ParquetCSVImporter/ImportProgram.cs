d7b25638e522b50efce29c1c5e1157f1b6eb1403
mxashlynn <mx.ashlynn@gmail.com>
2019-08-25 13:44:08 -0700

    Adds missing details to ImportProgram -- implementation incomplete.

--- ParquetCSVImporter/ImportProgram.cs
+++ ParquetCSVImporter/ImportProgram.cs
@@ -6,7 +6,12 @@ using System.Text;
 using CsvHelper;
 using CsvHelper.TypeConversion;
 using ParquetClassLibrary;
+using ParquetClassLibrary.Biomes;
+using ParquetClassLibrary.Characters;
+using ParquetClassLibrary.Crafting;
+using ParquetClassLibrary.Items;
 using ParquetClassLibrary.Parquets;
+using ParquetClassLibrary.Quests;
 using ParquetClassLibrary.Rooms;
 using ParquetCSVImporter.ClassMaps;
 
@@ -25,13 +30,29 @@ namespace ParquetCSVImporter
             Directory.GetCurrentDirectory().FullName;
 #endif
 
+
+        /// <summary>All <see cref="Being"/>s defined in the CSV files.</summary>
+        public static readonly HashSet<Being> Beings = new HashSet<Being>();
+
         /// <summary>All parquets defined in the CSV files.</summary>
         public static readonly HashSet<ParquetParent> Parquets = new HashSet<ParquetParent>();
 
         /// <summary>All <see cref="RoomRecipe"/>s defined in the CSV files.</summary>
         public static readonly HashSet<RoomRecipe> RoomRecipes = new HashSet<RoomRecipe>();
 
-        /// <summary>Instructions for handling integer type conversion when reading in parquet identifiers.</summary>
+        /// <summary>All <see cref="CraftingRecipe"/>s defined in the CSV files.</summary>
+        public static readonly HashSet<CraftingRecipe> CraftingRecipes = new HashSet<CraftingRecipe>();
+
+        /// <summary>All <see cref="Quest"/>s defined in the CSV files.</summary>
+        public static readonly HashSet<Quest> Quests = new HashSet<Quest>();
+
+        /// <summary>All <see cref="Biome"/>s defined in the CSV files.</summary>
+        public static readonly HashSet<Biome> Biomes = new HashSet<Biome>();
+
+        /// <summary>All <see cref="Item"/>s defined in the CSV files.</summary>
+        public static readonly HashSet<Item> Items = new HashSet<Item>();
+
+        /// <summary>Instructions for handling integer type conversion when reading in identifiers.</summary>
         public static readonly TypeConverterOptions IdentifierOptions = new TypeConverterOptions
         {
             NumberStyle = NumberStyles.AllowLeadingSign &
@@ -43,45 +64,72 @@ namespace ParquetCSVImporter
         /// </summary>
         public static void Main()
         {
-            var recordsFromCSV = new List<ParquetParent>();
-            recordsFromCSV.AddRange(GetRecordsForType<Floor>() ?? Enumerable.Empty<Floor>());
-            recordsFromCSV.AddRange(GetRecordsForType<Block>() ?? Enumerable.Empty<Block>());
-            recordsFromCSV.AddRange(GetRecordsForType<Furnishing>() ?? Enumerable.Empty<Furnishing>());
-            recordsFromCSV.AddRange(GetRecordsForType<Collectible>() ?? Enumerable.Empty<Collectible>());
+            #region Deserialization from CSV
 
+            #region Beings
+
+            var beingRecordsFromCSV = new List<Being>();
+            // TODO Unresolved design question -- do we predefine players, or are they all defined at runtime?
+            beingRecordsFromCSV.AddRange(GetBeingRecordsForType<PlayerCharacter>() ?? Enumerable.Empty<PlayerCharacter>());
+            beingRecordsFromCSV.AddRange(GetBeingRecordsForType<Critter>() ?? Enumerable.Empty<Critter>());
+            beingRecordsFromCSV.AddRange(GetBeingRecordsForType<NPC>() ?? Enumerable.Empty<NPC>());
+            Beings.Clear();
+            Beings.UnionWith(beingRecordsFromCSV);
+            #endregion
+
+            #region Parquets
+            var parquetRecordsFromCSV = new List<ParquetParent>();
+            parquetRecordsFromCSV.AddRange(GetParquetRecordsForType<Floor>() ?? Enumerable.Empty<Floor>());
+            parquetRecordsFromCSV.AddRange(GetParquetRecordsForType<Block>() ?? Enumerable.Empty<Block>());
+            parquetRecordsFromCSV.AddRange(GetParquetRecordsForType<Furnishing>() ?? Enumerable.Empty<Furnishing>());
+            parquetRecordsFromCSV.AddRange(GetParquetRecordsForType<Collectible>() ?? Enumerable.Empty<Collectible>());
             Parquets.Clear();
-            Parquets.UnionWith(recordsFromCSV);
+            Parquets.UnionWith(parquetRecordsFromCSV);
+            #endregion
 
-            All.InitializeCollections(Parquets, RoomRecipes);
+            #region Room Recipes
 
+            #endregion
+
+            #region Crafting Recipes
+
+            #endregion
+
+            #region Quests
+
+            #endregion
+
+            #region Biomes
+
+            #endregion
+
+            #region Items
+
+            #endregion
+
+            #endregion
+
+            #region Reserialization as JSON
+            All.InitializeCollections(Beings, Parquets, RoomRecipes, CraftingRecipes, Quests, Biomes, Items);
             var recordsToJSON = All.Parquets.SerializeToString();
-
-            OutputRecords(recordsToJSON);
-        }
-
-        /// <summary>
-        /// Reads all records of the given type from the appropriate file.
-        /// </summary>
-        /// <param name="in_jsonRecords">In JSON records to write.</param>
-        private static void OutputRecords(string in_jsonRecords)
-        {
             var filenameAndPath = Path.Combine(SearchPath, "Designer/Parquets.json");
             using (var writer = new StreamWriter(filenameAndPath, false, Encoding.UTF8))
             {
-                writer.Write(in_jsonRecords);
+                writer.Write(recordsToJSON);
             }
+            #endregion
         }
 
         /// <summary>
         /// Reads all records of the given type from the appropriate file.
         /// </summary>
-        /// <typeparam name="T">The type of records to read.</typeparam>
+        /// <typeparam name="BeingType">The type of records to read.</typeparam>
         /// <returns>The records read.</returns>
-        private static IEnumerable<T> GetRecordsForType<T>()
-            where T : ParquetParent
+        private static IEnumerable<BeingType> GetBeingRecordsForType<BeingType>()
+            where BeingType : Being
         {
-            IEnumerable<T> records;
-            var filenameAndPath = Path.Combine(SearchPath, $"Designer/{typeof(T).Name}.csv");
+            IEnumerable<BeingType> records;
+            var filenameAndPath = Path.Combine(SearchPath, $"Designer/{typeof(BeingType).Name}.csv");
             using (var reader = new StreamReader(filenameAndPath))
             {
                 using (var csv = new CsvReader(reader))
@@ -89,8 +137,33 @@ namespace ParquetCSVImporter
                     csv.Configuration.TypeConverterCache.AddConverter(typeof(EntityTag), new EntityTagConverter());
                     csv.Configuration.TypeConverterCache.AddConverter(typeof(EntityID), new EntityIDConverter());
                     csv.Configuration.TypeConverterOptionsCache.AddOptions(typeof(EntityID), IdentifierOptions);
-                    csv.Configuration.RegisterClassMapFor<T>();
-                    records = csv.GetRecordsViaShim<T>();
+                    csv.Configuration.RegisterClassMapFor<BeingType>();
+                    records = csv.GetRecordsViaShim<BeingType>();
+                }
+            }
+
+            return records;
+        }
+
+        /// <summary>
+        /// Reads all records of the given type from the appropriate file.
+        /// </summary>
+        /// <typeparam name="ParquetType">The type of records to read.</typeparam>
+        /// <returns>The records read.</returns>
+        private static IEnumerable<ParquetType> GetParquetRecordsForType<ParquetType>()
+            where ParquetType : ParquetParent
+        {
+            IEnumerable<ParquetType> records;
+            var filenameAndPath = Path.Combine(SearchPath, $"Designer/{typeof(ParquetType).Name}.csv");
+            using (var reader = new StreamReader(filenameAndPath))
+            {
+                using (var csv = new CsvReader(reader))
+                {
+                    csv.Configuration.TypeConverterCache.AddConverter(typeof(EntityTag), new EntityTagConverter());
+                    csv.Configuration.TypeConverterCache.AddConverter(typeof(EntityID), new EntityIDConverter());
+                    csv.Configuration.TypeConverterOptionsCache.AddOptions(typeof(EntityID), IdentifierOptions);
+                    csv.Configuration.RegisterClassMapFor<ParquetType>();
+                    records = csv.GetRecordsViaShim<ParquetType>();
                 }
             }
 
