34ffdb79e2b5ca1412fa892ede6dca8670eaf701
mxashlynn <mx.ashlynn@gmail.com>
2019-08-25 13:31:05 -0700

    Adds Quest stub class.

---
+++ ParquetClassLibrary/Quests/Quest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ParquetClassLibrary.Items;
+using ParquetClassLibrary.Utilities;
+
+namespace ParquetClassLibrary.Quests
+{
+    /// <summary>
+    /// Models a quest that an <see cref="Characters.NPC"/> may give to a <see cref="Characters.PlayerCharacter"/> embodies.
+    /// </summary>
+    // TODO This class is a place holder for a later implementation.
+    public class Quest : Entity
+    {
+        #region Characteristics
+        /// <summary>
+        /// Describes the criteria for completing this <see cref="Quest"/>.
+        /// </summary>
+        public IReadOnlyList<EntityTag> CompletionRequirements { get; }
+        #endregion
+
+        #region Initialization
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Quest"/> class.
+        /// </summary>
+        /// <param name="in_id">Unique identifier for the <see cref="Quest"/>.  Cannot be null.</param>
+        /// <param name="in_name">Player-friendly name of the <see cref="Quest"/>.  Cannot be null or empty.</param>
+        /// <param name="in_description">Player-friendly description of the <see cref="Quest"/>.</param>
+        /// <param name="in_comment">Comment of, on, or by the <see cref="Quest"/>.</param>
+        /// <param name="in_completionRequirements">Describes the criteria for completing this <see cref="Quest"/>.</param>
+        public Quest(EntityID in_id, string in_name, string in_description, string in_comment,
+                     List<EntityTag> in_completionRequirements)
+            : base(All.QuestIDs, in_id, in_name, in_description, in_comment)
+        {
+            CompletionRequirements = (in_completionRequirements ?? Enumerable.Empty<EntityTag>()).ToList();
+        }
+        #endregion
+    }
+}
