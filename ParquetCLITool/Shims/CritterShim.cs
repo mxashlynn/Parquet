b2fe6616e8b3141504c2d4a316b20f75082acc98
mxashlynn <mx.ashlynn@gmail.com>
2020-01-06 13:47:36 -0800

    Implements Being-related shims.

---
+++ ParquetCLITool/Shims/CritterShim.cs
@@ -0,0 +1,26 @@
+using ParquetClassLibrary;
+using ParquetClassLibrary.Beings;
+using ParquetClassLibrary.Utilities;
+
+namespace ParquetCLITool.Shims
+{
+    /// <summary>
+    /// Provides a default public parameterless constructor for a
+    /// <see cref="Critter"/>-like class that CSVHelper can instantiate.
+    /// 
+    /// Provides the ability to generate a <see cref="Critter"/> from this class.
+    /// </summary>
+    public class CritterShim : BeingShim
+    {
+        /// <summary>
+        /// Converts a shim into the class it corresponds to.
+        /// </summary>
+        /// <typeparam name="TargetType">The type to convert this shim to.</typeparam>
+        /// <returns>An instance of a child class of <see cref="Enity"/>.</returns>
+        public override TargetType To<TargetType>()
+        {
+            Precondition.IsOfType<TargetType, Critter>(typeof(TargetType).ToString());
+
+            return (TargetType)(Entity)new Critter(ID, Name, Description, Comment, NativeBiome, PrimaryBehavior, Avoids, Seeks);
+    }
+}
---
+++ ParquetCLITool/Shims/PlayerCharacterShim.cs
@@ -0,0 +1,28 @@
+using ParquetClassLibrary;
+using ParquetClassLibrary.Beings;
+using ParquetClassLibrary.Utilities;
+
+namespace ParquetCLITool.Shims
+{
+    /// <summary>
+    /// Provides a default public parameterless constructor for a
+    /// <see cref="PlayerCharacter"/>-like class that CSVHelper can instantiate.
+    /// 
+    /// Provides the ability to generate a <see cref="PlayerCharacter"/> from this class.
+    /// </summary>
+    public class PlayerCharacterShim : CharacterShim
+    {
+        /// <summary>
+        /// Converts a shim into the class it corresponds to.
+        /// </summary>
+        /// <typeparam name="TargetType">The type to convert this shim to.</typeparam>
+        /// <returns>An instance of a child class of <see cref="Enity"/>.</returns>
+        public override TargetType To<TargetType>()
+        {
+            Precondition.IsOfType<TargetType, PlayerCharacter>(typeof(TargetType).ToString());
+
+            return (TargetType)(Entity)new PlayerCharacter(ID, PersonalName, FamilyName, Description, Comment,
+                                                           Pronoun, StoryCharacterID, StartingQuests, StartingInventory);
+        }
+    }
+}
