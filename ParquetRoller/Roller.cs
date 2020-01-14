12520805608bd7e0c2441fd24dc97980e257b5a9
mxashlynn <mx.ashlynn@gmail.com>
2020-01-13 20:22:16 -0800

    Adds a stub for pronoun echoing to roller.

--- ParquetRoller/Roller.cs
+++ ParquetRoller/Roller.cs
@@ -25,11 +25,11 @@ namespace ParquetRoller
 
 
         #region Console Messages
-        private static string DefaultMessage { get; } = "Usage: roller (command)\nUsage: roller list (property) [category]\n\nCommands:\n    -h|help                         Display detailed help.\n    -v|version                      Display version information.\n    -t|templates                    Write CSV templates to current directory.\n    -r|roll                         Prepare CSVs in current directory for use.\n    -l|list (property) [category]   Inspect CSV properties and echo results.\n\n    For information on properties and categories consult the detailed help.\n";
+        private static string DefaultMessage { get; } = "Usage: roller (command)\nUsage: roller list (property) [category]\n\nCommands:\n    -h|help                         Display detailed help.\n    -v|version                      Display version information.\n    -t|templates                    Write CSV templates to current directory.\n    -r|roll                         Prepare CSVs in current directory for use.\n    -p|pronouns                     List all defined pronoun groups.\n    -l|list (property) [category]   Inspect CSV properties and echo results.\n\n    For information on properties and categories consult the detailed help.\n";
 
         private static string VersionMessage { get; } = $"Version:\n    Roller      {AssemblyInfo.LibraryVersion.Remove(AssemblyInfo.LibraryVersion.Length - 2)}\n    Parquet     {AssemblyInfo.LibraryVersion}\n    Map Data    {AssemblyInfo.SupportedMapDataVersion}\n    Being Data  {AssemblyInfo.SupportedBeingDataVersion}\n";
 
-        private static string HelpMessage { get; } = $"    Roller is a tool for working with Parquet configuration files.\n    Parquet uses comma-separated value (CSV) files for configuration.\n    Roller provides a quick way to examine the content of existing CSV files, to\n    generate blank CSV files, and to prepare existing CSV files for use in-game.\n\nUsage: roller (command)\nUsage: roller list (property) [category]\n\nCommands:\n    -h|help                         Display detailed help.\n    -v|version                      Display version information.\n    -t|templates                    Write CSV templates to current directory.\n    -r|roll                         Prepare CSVs in current directory for use.\n    -l|list (property) [category]   Inspect CSV properties and echo results.\n\nProperties:\n    ranges            Entity ID ranges valid for the given category.\n    maxids            The largest entity ID in use in the given category.\n    tags              All entity tags referenced in the given category.\n    names             All entity names referenced in the given category.\n    collisions        Any duplicate names used in the given category.\n\nCategories:\n    all               Everything, the default.  This can be a long listing.\n    beings            All beings.\n      critters        Only critter beings.\n      npcs            Only NPC beings.\n    biomes            All biomes.\n    crafts            All crafting recipes.\n    interactions      All interactions.\n      dialgoues       Only dialogue interactions.\n      quests          Only quest interactions.\n    items             All items.\n      p-items         Only items that correspond to parquets.\n      n-items         Only items that don't correspond to parquets.\n    parquets          All parquets.\n      floors          Only floor parquets.\n      blocks          Only block parquets.\n      furnishings     Only furnishing parquets.\n      collectibles    Only collectible parquets.\n    rooms             All room recipes.\n\n    Checking for name collisions is especially useful because they can cause\n    runtime errors.\n\n    \"Roller -- The Best Alternative to a 10-Pound Mallet!\"";
+        private static string HelpMessage { get; } = $"    Roller is a tool for working with Parquet configuration files.\n    Parquet uses comma-separated value (CSV) files for configuration.\n    Roller provides a quick way to examine the content of existing CSV files, to\n    generate blank CSV files, and to prepare existing CSV files for use in-game.\n\nUsage: roller (command)\nUsage: roller list (property) [category]\n\nCommands:\n    -h|help                         Display detailed help.\n    -v|version                      Display version information.\n    -t|templates                    Write CSV templates to current directory.\n    -r|roll                         Prepare CSVs in current directory for use.\n    -l|list (property) [category]   Inspect CSV properties and echo results.\n\nProperties:\n    ranges            Entity ID ranges valid for the given category.\n    maxids            The largest entity ID in use in the given category.\n    tags              All entity tags referenced in the given category.\n    names             All entity names referenced in the given category.\n    collisions        Any duplicate names used in the given category.\n\nCategories:\n    all               Everything, the default.  This can be a long listing.\n    beings            All beings.\n      critters        Only critter beings.\n      players         Only player character beings.\n      npcs            Only NPC beings.\n    biomes            All biomes.\n    crafts            All crafting recipes.\n    interactions      All interactions.\n      dialgoues       Only dialogue interactions.\n      quests          Only quest interactions.\n    items             All items.\n      p-items         Only items that correspond to parquets.\n      n-items         Only items that don't correspond to parquets.\n    parquets          All parquets.\n      floors          Only floor parquets.\n      blocks          Only block parquets.\n      furnishings     Only furnishing parquets.\n      collectibles    Only collectible parquets.\n    rooms             All room recipes.\n\n    Checking for name collisions is especially useful because they can cause\n    runtime errors.\n\n    \"Roller -- The Best Alternative to a 10-Pound Mallet!\"";
         #endregion
 
         /// <summary>
@@ -89,6 +89,10 @@ namespace ParquetRoller
                 case "-r":
                 case "roll":
                     return RollCSVs;
+                case "-p":
+                case "pronoun":
+                case "pronouns":
+                    return ListPronouns;
                 case "-l":
                 case "list":
                     return ListPropertyForCategory;
@@ -173,6 +177,11 @@ namespace ParquetRoller
                 case "critters":
                     var critters = All.Beings.Where(model => model.ID.IsValidForRange(All.CritterIDs));
                     workload = new ModelCollection(All.CritterIDs, critters);
+                    break;
+                case "player":
+                case "players":
+                    var players = All.Beings.Where(model => model.ID.IsValidForRange(All.PlayerCharacterIDs));
+                    workload = new ModelCollection(All.NpcIDs, players);
                     break;
                 case "npc":
                 case "npcs":
@@ -306,6 +315,18 @@ namespace ParquetRoller
         {
             // TODO This is a stub.
             Console.WriteLine("> Roll CSVs.");
+            return ExitCode.Success;
+        }
+
+        /// <summary>
+        /// List all defined pronoun groups.
+        /// </summary>
+        /// <param name="inWorkload">Ignored.</param>
+        /// <returns>A value indicating success or the nature of the failure.</returns>
+        private static ExitCode ListPronouns(ModelCollection inWorkload)
+        {
+            // TODO This is a stub.
+            Console.WriteLine("> List pronoun groups.");
             return ExitCode.Success;
         }
         #endregion
