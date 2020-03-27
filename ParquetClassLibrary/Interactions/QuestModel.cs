34b1a9ff065fb7cf21a4f69f28b62bd8617e4007
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-03-26 18:34:20 -0700

    Finished implementing QuestModel.

--- ParquetClassLibrary/Interactions/QuestModel.cs
+++ ParquetClassLibrary/Interactions/QuestModel.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
-using CsvHelper.Configuration.Attributes;
 
 namespace ParquetClassLibrary.Interactions
 {
@@ -9,15 +7,6 @@ namespace ParquetClassLibrary.Interactions
     /// </summary>
     public sealed class QuestModel : InteractionModel
     {
-        #region Characteristics
-        // TODO Is this completely implemented?  Check paper notes.
-        /// <summary>
-        /// Describes the criteria for completing this <see cref="QuestModel"/>.
-        /// </summary>
-        [Index(7)]
-        public IReadOnlyList<ModelTag> CompletionRequirements { get; }
-        #endregion
-
         #region Initialization
         /// <summary>
         /// Initializes a new instance of the <see cref="QuestModel"/> class.
@@ -26,17 +15,13 @@ namespace ParquetClassLibrary.Interactions
         /// <param name="inName">Player-friendly name of the <see cref="QuestModel"/>.  Cannot be null or empty.</param>
         /// <param name="inDescription">Player-friendly description of the <see cref="QuestModel"/>.</param>
         /// <param name="inComment">Comment of, on, or by the <see cref="QuestModel"/>.</param>
-        /// <param name="inStartCriteria">Describes the criteria for completing this <see cref="DialogueModel"/>.</param>
-        /// <param name="inSteps">Describes the criteria for completing this <see cref="DialogueModel"/>.</param>
-        /// <param name="inOutcome">Describes the criteria for completing this <see cref="DialogueModel"/>.</param>
-        /// <param name="inCompletionRequirements">Describes the criteria for completing this <see cref="QuestModel"/>.</param>
+        /// <param name="inPrerequisites">Describes the criteria for beginning this <see cref="QuestModel"/>.</param>
+        /// <param name="inSteps">Describes the criteria for completing this <see cref="QuestModel"/>.</param>
+        /// <param name="inOutcome">Describes the results of completing this <see cref="QuestModel"/>.</param>
         public QuestModel(ModelID inID, string inName, string inDescription, string inComment,
-                          IEnumerable<ModelTag> inStartCriteria, IEnumerable<ModelTag> inSteps, string inOutcome,
-                          IEnumerable<ModelTag> inCompletionRequirements)
-            : base(All.QuestIDs, inID, inName, inDescription, inComment, inStartCriteria, inSteps, inOutcome)
-        {
-            CompletionRequirements = (inCompletionRequirements ?? Enumerable.Empty<ModelTag>()).ToList();
-        }
+                          IEnumerable<ModelID> inPrerequisites, IEnumerable<ModelID> inSteps, IEnumerable<ModelID> inOutcome)
+            : base(All.QuestIDs, inID, inName, inDescription, inComment, inPrerequisites, inSteps, inOutcome)
+        { }
         #endregion
     }
 }
