553287f667e5209d4baf0de55bdeb12537569b92
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-04-30 17:56:41 -0700

    Adds missing data version unit tests.

--- ParquetUnitTests/AssemblyInfoUnitTest.cs
+++ ParquetUnitTests/AssemblyInfoUnitTest.cs
@@ -7,11 +7,23 @@ namespace ParquetUnitTests
     {
         #region Values for Tests
         /// <summary>This is the cannonical invalid data version string used in serialization tests.</summary>
-        private const string invalidDataVersion = "0.0.0";
+        private const string invalidDataVersion = "0.0";
 
         /// <summary>This is the cannonical invalid library version string used in serialization tests.</summary>
         private const string invalidLibVersion = "0.0.0.0";
         #endregion
+
+        [Fact]
+        public void SupportedCharacterDataVersionIsDefinedTest()
+        {
+            Assert.False(string.IsNullOrEmpty(AssemblyInfo.SupportedBeingDataVersion));
+        }
+
+        [Fact]
+        public void SupportedCharacterDataVersionIsNotInvalidTest()
+        {
+            Assert.NotEqual(invalidDataVersion, AssemblyInfo.SupportedBeingDataVersion);
+        }
 
         [Fact]
         public void SupportedMapDataVersionIsDefinedTest()
@@ -26,15 +38,15 @@ namespace ParquetUnitTests
         }
 
         [Fact]
-        public void SupportedCharacterDataVersionIsDefinedTest()
+        public void SupportedScriptDataVersionIsDefinedTest()
         {
-            Assert.False(string.IsNullOrEmpty(AssemblyInfo.SupportedBeingDataVersion));
+            Assert.False(string.IsNullOrEmpty(AssemblyInfo.SupportedScriptDataVersion));
         }
 
         [Fact]
-        public void SupportedCharacterDataVersionIsNotInvalidTest()
+        public void SupportedScriptDataVersionIsNotInvalidTest()
         {
-            Assert.NotEqual(invalidDataVersion, AssemblyInfo.SupportedBeingDataVersion);
+            Assert.NotEqual(invalidDataVersion, AssemblyInfo.SupportedScriptpDataVersion);
         }
 
         [Fact]
