0d62d4da1deaf6d037f6b6768a3ce8b0d53b734e
Paige Ashlynn <mxashlynn@users.noreply.github.com>
2020-05-10 14:49:25 -0700

    Fixes name and adds room-to-room communication.

--- ParquetClassLibrary/Parquets/EntryType.cs
+++ ParquetClassLibrary/Parquets/EntryType.cs
@@ -3,10 +3,12 @@ namespace ParquetClassLibrary.Scripts
     /// <summary>
     /// Whether and how a <see cref="Furnishing"/> communicates to an adjacent <see cref="Maps.MapRegion"/>.
     /// </summary>
-    public enum ExitType
+    public enum EntryType
     {
-        /// <summary>This furnishing does not communicate to another map.</summary>
+        /// <summary>This furnishing does not communicate to another map or room.</summary>
         None,
+        /// <summary>This furnishing communicates between rooms on the same map.</summary>
+        Room,
         /// <summary>This furnishing communicates to the map above.</summary>
         Up,
         /// <summary>This furnishing communicates to the map below.</summary>
