0e045d6c77a17358ce63a722e4a1693de539efa3
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-05-18 15:37:33 -0700

    Implements region-Composition proof-of-concept.

--- ParquetRunner/RunnerProgram.cs
+++ ParquetRunner/RunnerProgram.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ParquetClassLibrary;
 using ParquetClassLibrary.Beings;
 using ParquetClassLibrary.Biomes;
@@ -24,9 +25,28 @@ namespace ParquetRunner
         {
             All.LoadFromCSVs();
 
+            /*
             var region = new MapRegion(All.MapRegionIDs.Minimum, "Sample Region");
             Console.WriteLine(region);
             Console.WriteLine($"Item range = {All.ItemIDs}");
+            */
+
+            var procGenChunk = 70001;
+            var chunks = new ModelIDGrid(MapRegionSketch.ChunksPerRegionDimension, MapRegionSketch.ChunksPerRegionDimension);
+            for (var x = 0; x < MapRegionSketch.ChunksPerRegionDimension; x++)
+            {
+                for (var y = 0; y < MapRegionSketch.ChunksPerRegionDimension; y++)
+                {
+                    chunks[y, x] = procGenChunk;
+                }
+            }
+            var sketch = new MapRegionSketch(All.MapRegionIDs.Minimum, ": Region Composition Proof-of-Concept", "", "", 1, "#AAAAAAFF",
+                                             ModelID.None, ModelID.None, ModelID.None, ModelID.None, ModelID.None, ModelID.None,
+                                             chunks);
+
+            Console.WriteLine(sketch);
+            var region = sketch.Stitch();
+            Console.WriteLine(region);
 
             All.SaveToCSVs();
         }
