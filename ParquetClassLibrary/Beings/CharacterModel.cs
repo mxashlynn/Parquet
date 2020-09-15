f313bd2f00e1b3e42709900b143ac248bbfb16a3
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-09-15 12:47:41 -0700

    Removes obsolete using directives.

--- ParquetClassLibrary/Beings/CharacterModel.cs
+++ ParquetClassLibrary/Beings/CharacterModel.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using CsvHelper.Configuration.Attributes;
 using ParquetClassLibrary.EditorSupport;
-using ParquetClassLibrary.Properties;
 
 namespace ParquetClassLibrary.Beings
 {
--- ParquetClassLibrary/Maps/MapChunkModel.cs
+++ ParquetClassLibrary/Maps/MapChunkModel.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using CsvHelper.Configuration.Attributes;
 using ParquetClassLibrary.EditorSupport;
 using ParquetClassLibrary.Parquets;
