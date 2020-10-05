1e04a08f6546d432a8a69dd7965dfd505526385e
Paige Ashlynn <mx.ashlynn@gmail.com>
2020-10-05 13:16:56 -0700

    Simplifies room detection code.
    Replaces RoomCollection with ParquetStackGridExtensions and ReadOnlyRoomCollectionExtensions, collapsing nested namespaces in the process.

--- ParquetClassLibrary/Rooms/ParquetStackGridExtensions.cs
+++ ParquetClassLibrary/Rooms/ParquetStackGridExtensions.cs
@@ -1,134 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using ParquetClassLibrary.Parquets;
 
 namespace ParquetClassLibrary.Rooms
 {
-    // Local extension methods allow fluent algorithm expression.  See bottom of this file for definitions.
-    using ParquetClassLibrary.Rooms.RegionAnalysis;
-
     /// <summary>
-    /// Stores a <see cref="Room"/> collection.
-    /// Analyzes subregions of <see cref="ParquetStack"/>s to find all valid rooms within them.
+    /// Extensions to <see cref="ParquetStackGrid"/> for analyzing subregions of <see cref="ParquetStack"/>s
+    /// to find all valid <see cref="Room"/>s within them.
     /// </summary>
     /// <remarks>
     /// For a complete explanation of the algorithm implemented here, see:
     /// <a href="https://github.com/mxashlynn/Parquet/blob/master/Documentation/4.-Room_Detection_and_Type_Assignment.md"/>
     /// </remarks>
-    public class RoomCollection : IReadOnlyCollection<Room>
+    public static class ParquetStackGridExtensions
     {
-        /// <summary>The internal collection mechanism.</summary>
-        private IReadOnlyList<Room> Rooms { get; }
-
-        /// <summary>The number of <see cref="Room"/>s in the <see cref="RoomCollection"/>.</summary>
-        public int Count
-            => Rooms?.Count ?? 0;
-
         /// <summary>
-        /// Determines whether the <see cref="RoomCollection"/> contains the specified <see cref="Room"/>.
+        /// Initializes a new <see cref="IReadOnlyCollection{Room}"/> from the current <see cref="ParquetStackGrid"/>.
         /// </summary>
-        /// <param name="inRoom">The <see cref="Room"/> to find.</param>
-        /// <returns><c>true</c> if the <see cref="Room"/> was found; <c>false</c> otherwise.</returns>
-        public bool Contains(Room inRoom)
-            => Rooms.Contains(inRoom);
-
-        /// <summary>
-        /// Returns the <see cref="Room"/> at the given position, if there is one.
-        /// </summary>
-        /// <param name="inPosition">An in-bounds position to search for a <see cref="Room"/>.</param>
-        /// <returns>The specified <see cref="Room"/> if found; otherwise, null.</returns>
-        public Room GetRoomAt(Vector2D inPosition)
-            => Rooms.FirstOrDefault(room => room.ContainsPosition(inPosition));
-
-        #region Initialization
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RoomCollection"/> class.
-        /// </summary>
-        /// <remarks>Private so that empty <see cref="RoomCollection"/>s are not made in client code.</remarks>
-        private RoomCollection(IEnumerable<Room> inRooms)
-            => Rooms = inRooms.ToList();
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="RoomCollection"/> class.
-        /// </summary>
-        /// <param name="inSubregion">The collection of parquets to search for <see cref="Room"/>s.</param>
-        /// <returns>An initialized <see cref="RoomCollection"/>.</returns>
-        public static RoomCollection CreateFromSubregion(ParquetStackGrid inSubregion)
+        /// <param name="inSubregion">The current collection of parquets to search for <see cref="Room"/>s.</param>
+        /// <returns>An initialized collection of rooms.</returns>
+        public static IReadOnlyCollection<Room> CreateRoomCollectionFromSubregion(this ParquetStackGrid inSubregion)
         {
             Precondition.IsNotNull(inSubregion, nameof(inSubregion));
 
-            var walkableAreas = inSubregion.GetWalkableAreas();
+            var walkableAreas = GetWalkableAreas(inSubregion);
             var perimeter = MapSpaceCollection.Empty;
             var rooms =
                 walkableAreas
                 .Where(walkableArea => walkableArea.TryGetPerimeter(out perimeter)
                                     && walkableArea.Concat(perimeter)
                                                    .Any(space => space.Content.FurnishingID != ModelID.None
-                                                              && space.Content.FurnishingID.IsEntryFurnishing())
+                                                              && IsEntryFurnishing(space.Content.FurnishingID))
                                     && walkableArea.Any(space => space.IsWalkableEntry
                                                               || space.Neighbors().Any(neighbor => neighbor.IsEnclosingEntry(walkableArea))))
                 .Select(walkableArea => new Room(walkableArea, perimeter));
 
-            return new RoomCollection(rooms);
+            return rooms.ToList();
         }
-        #endregion
 
-        #region Collection Access
-        /// <summary>
-        /// Exposes an <see cref="IEnumerator"/>, which supports simple iteration.
-        /// </summary>
-        /// <returns>An enumerator.</returns>
-        IEnumerator IEnumerable.GetEnumerator()
-            => Rooms.GetEnumerator();
-
-        /// <summary>
-        /// Retrieves an enumerator for the <see cref="RoomCollection"/>.
-        /// </summary>
-        /// <returns>An enumerator that iterates through the collection.</returns>
-        public IEnumerator<Room> GetEnumerator()
-            => Rooms.GetEnumerator();
-        #endregion
-
-        #region Utilities
-        /// <summary>
-        /// Returns a <see cref="string"/> that represents the current <see cref="RoomCollection"/>.
-        /// </summary>
-        /// <returns>The representation.</returns>
-        public override string ToString()
-            => $"{Rooms.Count} rooms";
-        #endregion
-    }
-}
-
-namespace ParquetClassLibrary.Rooms.RegionAnalysis
-{
-    /// <summary>
-    /// Extension methods used only by <see cref="RoomCollection"/> when analyzing <see cref="Model"/>s.
-    /// </summary>
-    internal static class ModelIDExtensions
-    {
-        /// <summary>
-        /// Determines if the current <see cref="ModelID"/> refers to a <see cref="FurnishingModel"/> whose
-        /// entry is not <see cref="EntryType.None"/>.
-        /// </summary>
-        /// <param name="inFurnishingID">The <see cref="ModelID"/> to evaluate.</param>
-        /// <returns><c>true</c> if the furnishing is an Entry furnishing; <c>false</c> otherwise.</returns>
-        internal static bool IsEntryFurnishing(this ModelID inFurnishingID)
-            => (All.Furnishings.Get<FurnishingModel>(inFurnishingID)?.Entry ?? EntryType.None) != EntryType.None;
-    }
-
-    /// <summary>
-    /// Extension methods used only by <see cref="RoomCollection"/> when analyzing subregions of <see cref="MapSpace"/>s.
-    /// </summary>
-    internal static class ParquetStackExtensions
-    {
         /// <summary>
         /// Finds all valid Walkable Areas in a given subregion.
         /// </summary>
         /// <param name="inSubregion">The <see cref="ParquetStackGrid"/>s to search.</param>
         /// <returns>The list of vallid Walkable Areas.</returns>
-        internal static IReadOnlyList<MapSpaceCollection> GetWalkableAreas(this ParquetStackGrid inSubregion)
+        private static IReadOnlyList<MapSpaceCollection> GetWalkableAreas(ParquetStackGrid inSubregion)
         {
             var PWAs = new List<HashSet<MapSpace>>();
             var subregionRows = inSubregion.Rows;
@@ -189,5 +104,14 @@ namespace ParquetClassLibrary.Rooms.RegionAnalysis
 
             return results.ConvertAll(hashOfSpaces => new MapSpaceCollection(hashOfSpaces));
         }
+
+        /// <summary>
+        /// Determines if the given <see cref="ModelID"/> refers to a <see cref="FurnishingModel"/> whose
+        /// entry is not <see cref="EntryType.None"/>.
+        /// </summary>
+        /// <param name="inFurnishingID">The <see cref="ModelID"/> to evaluate.</param>
+        /// <returns><c>true</c> if the furnishing is an Entry furnishing; <c>false</c> otherwise.</returns>
+        private static bool IsEntryFurnishing(ModelID inFurnishingID)
+            => (All.Furnishings.Get<FurnishingModel>(inFurnishingID)?.Entry ?? EntryType.None) != EntryType.None;
     }
 }
---
+++ ParquetClassLibrary/Rooms/ReadOnlyRoomCollectionExtensions.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParquetClassLibrary.Rooms
+{
+    /// <summary>
+    /// Convenience extensions to <see cref="IReadOnlyCollection{Room}"/>.
+    /// </summary>
+    public static class ReadOnlyRoomCollectionExtensions
+    {
+        /// <summary>
+        /// Returns the <see cref="Room"/> at the given position, if there is one.
+        /// </summary>
+        /// <param name="inRooms">The current collection of <see cref="Room"/>s.</param>
+        /// <param name="inPosition">An in-bounds position to search for a <see cref="Room"/>.</param>
+        /// <returns>The specified <see cref="Room"/> if found; otherwise, null.</returns>
+        public static Room GetRoomAt(this IReadOnlyList<Room> inRooms, Vector2D inPosition)
+            => inRooms.FirstOrDefault(room => room.ContainsPosition(inPosition));
+
+        /// <summary>
+        /// Returns a <see cref="string"/> that represents the current <see cref="IReadOnlyCollection{Room}"/>.
+        /// </summary>
+        /// <returns>The representation.</returns>
+        public static string ToString(this IReadOnlyCollection<Room> inRooms)
+            => $"{inRooms?.Count ?? 0} rooms";
+    }
+}
