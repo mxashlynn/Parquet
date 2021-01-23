a96bcd069aa5660dc12343efb1121d55e0ca9192
MxAshlynn <mx.ashlynn@gmail.com>
2021-01-22 19:08:59 -0800

    Refactors code to eliminate suppressions.

--- ParquetClassLibrary/All.cs
+++ ParquetClassLibrary/All.cs
@@ -660,14 +660,9 @@ namespace ParquetClassLibrary
         /// Note that this method is only available when Parquet is built with editor support enabled.
         /// This means that when games that do not support live editing of models must initialize <see cref="All"/> only once per run.
         /// </remarks>
-        [SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression",
-                         Justification = "Suppression is needed when DESIGN is undefined.")]
-        [SuppressMessage("Style", "IDE0022:Use expression body for methods",
-                         Justification = "Cannot be an expression when symbol DESIGN is defined.")]
-        // TODO The above suppression pair indicates that there is likely a better way to handle this method.
+#if DESIGN
         public static void Clear()
         {
-#if DESIGN
             ((EditorSupport.IMutableModelCollection<GameModel>)Games)?.Clear();
             ((EditorSupport.IMutableModelCollection<FloorModel>)Floors)?.Clear();
             ((EditorSupport.IMutableModelCollection<BlockModel>)Blocks)?.Clear();
@@ -684,10 +679,11 @@ namespace ParquetClassLibrary
             ((EditorSupport.IMutableModelCollection<ItemModel>)Items)?.Clear();
             ((HashSet<PronounGroup>)PronounGroups)?.Clear();
             CollectionsHaveBeenInitialized = false;
-#else
-            throw new InvalidOperationException(Resources.ErrorEditorSupport);
-#endif
         }
+#else
+        public static void Clear()
+            => throw new InvalidOperationException(Resources.ErrorEditorSupport);
+#endif
         #endregion
 
         #region ModelID Range Helper Methods
