175d460520caabafc9eff90423749439122ae4d2
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-16 16:15:30 -0800

    Renames InteractionStatus to ScriptStatus
    as it should be usable for both.

--- ParquetClassLibrary/Scripts/ScriptStatus.cs
+++ ParquetClassLibrary/Scripts/ScriptStatus.cs
@@ -6,13 +6,17 @@ using CsvHelper.Configuration;
 namespace Parquet.Scripts
 {
     /// <summary>
-    /// Tracks the status of an <see cref="InteractionModel"/>.
+    /// Tracks the status of a <see cref="ScriptModel"/>.
     /// </summary>
-    public class InteractionStatus : Status<InteractionModel>
+    /// <remarks>
+    /// This can also be used to tracks the status of an <see cref="InteractionModel"/>
+    /// as only <see cref="InteractionModel.StepsIDs"/> is stateful.
+    /// </remarks>
+    public class ScriptStatus : Status<ScriptModel>
     {
         #region Class Defaults
-        /// <summary>Provides an instance of the <see cref="InteractionStatus"/> class with default values.</summary>
-        public static InteractionStatus Unstarted { get; } = new InteractionStatus();
+        /// <summary>Provides an instance of the <see cref="ScriptStatus"/> class with default values.</summary>
+        public static ScriptStatus Unstarted { get; } = new ScriptStatus();
         #endregion
 
         #region Status
@@ -25,11 +29,11 @@ namespace Parquet.Scripts
 
         #region Initialization
         /// <summary>
-        /// Initializes a new instance of the <see cref="InteractionStatus"/> class.
+        /// Initializes a new instance of the <see cref="ScriptStatus"/> class.
         /// </summary>
-        /// <param name="inState">The <see cref="RunState"/> of the tracked <see cref="InteractionModel"/>.</param>
-        /// <param name="inProgramCounter">Index to the current <see cref="ScriptNode"/> in the tracked <see cref="InteractionModel.StepsIDs"/>.</param>
-        public InteractionStatus(RunState inState = RunState.Unstarted, int inProgramCounter = 0)
+        /// <param name="inState">The <see cref="RunState"/> of the tracked <see cref="ScriptModel"/>.</param>
+        /// <param name="inProgramCounter">Index to the current <see cref="ScriptNode"/> in the tracked <see cref="ScriptModel.StepsIDs"/>.</param>
+        public ScriptStatus(RunState inState = RunState.Unstarted, int inProgramCounter = 0)
         {
             State = inState;
             ProgramCounter = inProgramCounter;
@@ -47,46 +51,46 @@ namespace Parquet.Scripts
             => (State, ProgramCounter).GetHashCode();
 
         /// <summary>
-        /// Determines whether the specified <see cref="InteractionStatus"/> is equal to the current <see cref="InteractionStatus"/>.
+        /// Determines whether the specified <see cref="ScriptStatus"/> is equal to the current <see cref="ScriptStatus"/>.
         /// </summary>
-        /// <param name="inStatus">The <see cref="InteractionStatus"/> to compare with the current.</param>
+        /// <param name="inStatus">The <see cref="ScriptStatus"/> to compare with the current.</param>
         /// <returns><c>true</c> if they are equal; otherwise, <c>false</c>.</returns>
         public override bool Equals<T>(T inStatus)
-            => inStatus is InteractionStatus interactionStatus
-            && State == interactionStatus.State
-            && ProgramCounter == interactionStatus.ProgramCounter;
+            => inStatus is ScriptStatus ScriptStatus
+            && State == ScriptStatus.State
+            && ProgramCounter == ScriptStatus.ProgramCounter;
 
         /// <summary>
-        /// Determines whether the specified <see cref="object"/> is equal to the current <see cref="InteractionStatus"/>.
+        /// Determines whether the specified <see cref="object"/> is equal to the current <see cref="ScriptStatus"/>.
         /// </summary>
-        /// <param name="obj">The <see cref="object"/> to compare with the current <see cref="InteractionStatus"/>.</param>
+        /// <param name="obj">The <see cref="object"/> to compare with the current <see cref="ScriptStatus"/>.</param>
         /// <returns><c>true</c> if they are equal; otherwise, <c>false</c>.</returns>
         public override bool Equals(object obj)
-            => obj is InteractionStatus status
+            => obj is ScriptStatus status
             && Equals(status);
 
         /// <summary>
-        /// Determines whether a specified instance of <see cref="InteractionStatus"/> is equal to another specified instance of <see cref="InteractionStatus"/>.
+        /// Determines whether a specified instance of <see cref="ScriptStatus"/> is equal to another specified instance of <see cref="ScriptStatus"/>.
         /// </summary>
-        /// <param name="inStatus1">The first <see cref="InteractionStatus"/> to compare.</param>
-        /// <param name="inStatus2">The second <see cref="InteractionStatus"/> to compare.</param>
+        /// <param name="inStatus1">The first <see cref="ScriptStatus"/> to compare.</param>
+        /// <param name="inStatus2">The second <see cref="ScriptStatus"/> to compare.</param>
         /// <returns><c>true</c> if they are equal; otherwise, <c>false</c>.</returns>
-        public static bool operator ==(InteractionStatus inStatus1, InteractionStatus inStatus2)
+        public static bool operator ==(ScriptStatus inStatus1, ScriptStatus inStatus2)
             => inStatus1?.Equals(inStatus2) ?? inStatus2?.Equals(inStatus1) ?? true;
 
         /// <summary>
-        /// Determines whether a specified instance of <see cref="InteractionStatus"/> is not equal to another specified instance of <see cref="InteractionStatus"/>.
+        /// Determines whether a specified instance of <see cref="ScriptStatus"/> is not equal to another specified instance of <see cref="ScriptStatus"/>.
         /// </summary>
-        /// <param name="inStatus1">The first <see cref="InteractionStatus"/> to compare.</param>
-        /// <param name="inStatus2">The second <see cref="InteractionStatus"/> to compare.</param>
+        /// <param name="inStatus1">The first <see cref="ScriptStatus"/> to compare.</param>
+        /// <param name="inStatus2">The second <see cref="ScriptStatus"/> to compare.</param>
         /// <returns><c>true</c> if they are NOT equal; otherwise, <c>false</c>.</returns>
-        public static bool operator !=(InteractionStatus inStatus1, InteractionStatus inStatus2)
+        public static bool operator !=(ScriptStatus inStatus1, ScriptStatus inStatus2)
             => !(inStatus1 == inStatus2);
         #endregion
 
         #region ITypeConverter Implementation
         /// <summary>Allows the converter to construct itself statically.</summary>
-        internal static InteractionStatus ConverterFactory { get; } = Unstarted;
+        internal static ScriptStatus ConverterFactory { get; } = Unstarted;
 
         /// <summary>
         /// Converts the given <see cref="object"/> to a <see cref="string"/> for serialization.
@@ -96,10 +100,10 @@ namespace Parquet.Scripts
         /// <param name="inMemberMapData">Mapping info for a member to a CSV field or property.</param>
         /// <returns>The given instance serialized.</returns>
         public override string ConvertToString(object inValue, IWriterRow inRow, MemberMapData inMemberMapData)
-            => inValue is InteractionStatus status
+            => inValue is ScriptStatus status
                 ? $"{status.State}{Delimiters.InternalDelimiter}" +
                   $"{status.ProgramCounter}"
-                : Logger.DefaultWithConvertLog(inValue?.ToString() ?? "null", nameof(InteractionStatus), nameof(Unstarted));
+                : Logger.DefaultWithConvertLog(inValue?.ToString() ?? "null", nameof(ScriptStatus), nameof(Unstarted));
 
         /// <summary>
         /// Converts the given <see cref="string"/> to an <see cref="object"/> as deserialization.
@@ -126,17 +130,17 @@ namespace Parquet.Scripts
                 ? temp1
                 : Logger.DefaultWithParseLog(parameterText[1], nameof(ProgramCounter), 0);
 
-            return new InteractionStatus(parsedState, parsedProgramCounter);
+            return new ScriptStatus(parsedState, parsedProgramCounter);
         }
         #endregion
 
         #region Utilities
         /// <summary>
-        /// Returns a <see cref="string"/> that represents the current <see cref="InteractionStatus"/>.
+        /// Returns a <see cref="string"/> that represents the current <see cref="ScriptStatus"/>.
         /// </summary>
         /// <returns>The representation.</returns>
         public override string ToString()
-            => $"{nameof(InteractionStatus)}: {State}";
+            => $"{nameof(ScriptStatus)}: {State}";
         #endregion
     }
 }
