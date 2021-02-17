960dda4e37b4c45c7a435a8d812ae12920b6d60a
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-17 15:41:51 -0800

    Creates a GameStatus class.

---
+++ ParquetClassLibrary/Games/GameStatus.cs
@@ -0,0 +1,142 @@
+using System;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Parquet.Games
+{
+    /// <summary>
+    /// Tracks the status of a <see cref="GameModel"/>.
+    /// Instances of this class are mutable during play.
+    /// </summary>
+    public sealed class GameStatus : Status<GameModel>
+    {
+        #region Class Defaults
+        /// <summary>Provides an instance of the <see cref="GameStatus"/> class with default values.</summary>
+        public static GameStatus Default { get; } = new GameStatus();
+        #endregion
+
+        #region Status
+        /// <summary>The <see cref="ModelID"/> of the <see cref="Beings.CharacterModel"/> that the player controls.</summary>
+        public ModelID PlayerCharacterID { get; set; }
+
+        /// <summary>The <see cref="ModelID"/> of the <see cref="Scripts.ScriptModel"/> that is running.</summary>
+        public ModelID CurrentScriptID { get; set; }
+        #endregion
+
+        #region Initialization
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameStatus"/> class.
+        /// </summary>
+        /// <param name="inPlayerCharacterID">The <see cref="ModelID"/> of the <see cref="Beings.CharacterModel"/> that the player controls at the outset.</param>
+        /// <param name="inFirstScriptID">The <see cref="ModelID"/> of the <see cref="Scripts.ScriptModel"/> to run when play begins.</param>
+        public GameStatus(ModelID? inPlayerCharacterID = null, ModelID? inFirstScriptID = null)
+        {
+            PlayerCharacterID = inPlayerCharacterID ?? ModelID.None;
+            CurrentScriptID = inFirstScriptID ?? ModelID.None;
+        }
+        #endregion
+
+        #region IEquatable Implementation
+        /// <summary>
+        /// Serves as a hash function for a <see cref="GameStatus"/>.
+        /// </summary>
+        /// <returns>A hash code for this instance that is suitable for use in hashing algorithms and data structures.</returns>
+        public override int GetHashCode()
+            => (PlayerCharacterID, CurrentScriptID).GetHashCode();
+
+        /// <summary>
+        /// Determines whether the specified <see cref="GameStatus"/> is equal to the current <see cref="GameStatus"/>.
+        /// </summary>
+        /// <param name="inStatus">The <see cref="GameStatus"/> to compare with the current.</param>
+        /// <returns><c>true</c> if they are equal; otherwise, <c>false</c>.</returns>
+        public override bool Equals<T>(T inStatus)
+            => inStatus is GameStatus status
+            && PlayerCharacterID == status.PlayerCharacterID
+            && CurrentScriptID == status.CurrentScriptID;
+
+        /// <summary>
+        /// Determines whether the specified <see cref="object"/> is equal to the current <see cref="GameStatus"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="object"/> to compare with the current <see cref="GameStatus"/>.</param>
+        /// <returns><c>true</c> if they are equal; otherwise, <c>false</c>.</returns>
+        public override bool Equals(object obj)
+            => obj is GameStatus status
+            && Equals(status);
+
+        /// <summary>
+        /// Determines whether a specified instance of <see cref="GameStatus"/> is equal to another specified instance of <see cref="GameStatus"/>.
+        /// </summary>
+        /// <param name="inStatus1">The first <see cref="GameStatus"/> to compare.</param>
+        /// <param name="inStatus2">The second <see cref="GameStatus"/> to compare.</param>
+        /// <returns><c>true</c> if they are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(GameStatus inStatus1, GameStatus inStatus2)
+            => inStatus1?.Equals(inStatus2) ?? inStatus2?.Equals(inStatus1) ?? true;
+
+        /// <summary>
+        /// Determines whether a specified instance of <see cref="GameStatus"/> is not equal to another specified instance of <see cref="GameStatus"/>.
+        /// </summary>
+        /// <param name="inStatus1">The first <see cref="GameStatus"/> to compare.</param>
+        /// <param name="inStatus2">The second <see cref="GameStatus"/> to compare.</param>
+        /// <returns><c>true</c> if they are NOT equal; otherwise, <c>false</c>.</returns>
+        public static bool operator !=(GameStatus inStatus1, GameStatus inStatus2)
+            => !(inStatus1 == inStatus2);
+        #endregion
+
+        #region ITypeConverter Implementation
+        /// <summary>Allows the converter to construct itself statically.</summary>
+        internal static GameStatus ConverterFactory { get; } = Default;
+
+        /// <summary>
+        /// Converts the given <see cref="object"/> to a <see cref="string"/> for serialization.
+        /// </summary>
+        /// <param name="inValue">The instance to convert.</param>
+        /// <param name="inRow">The current context and configuration.</param>
+        /// <param name="inMemberMapData">Mapping info for a member to a CSV field or property.</param>
+        /// <returns>The given instance serialized.</returns>
+        public override string ConvertToString(object inValue, IWriterRow inRow, MemberMapData inMemberMapData)
+            => inValue is GameStatus status
+                ? $"{ModelID.ConverterFactory.ConvertToString(status.PlayerCharacterID, inRow, inMemberMapData)}{Delimiters.InternalDelimiter}" +
+                  $"{ModelID.ConverterFactory.ConvertToString(status.CurrentScriptID, inRow, inMemberMapData)}"
+                : Logger.DefaultWithConvertLog(inValue?.ToString() ?? "null", nameof(GameStatus), nameof(Default));
+
+        /// <summary>
+        /// Converts the given <see cref="string"/> to an <see cref="object"/> as deserialization.
+        /// </summary>
+        /// <param name="inText">The text to convert.</param>
+        /// <param name="inRow">The current context and configuration.</param>
+        /// <param name="inMemberMapData">Mapping info for a member to a CSV field or property.</param>
+        /// <returns>The given instance deserialized.</returns>
+        public override object ConvertFromString(string inText, IReaderRow inRow, MemberMapData inMemberMapData)
+        {
+            if (string.IsNullOrEmpty(inText)
+                || string.Compare(nameof(Default), inText, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                return Default.DeepClone();
+            }
+
+            var parameterText = inText.Split(Delimiters.InternalDelimiter);
+
+            var parsedPlayerCharacterID = (ModelID)ModelID.ConverterFactory.ConvertFromString(parameterText[0], inRow, inMemberMapData);
+            var parsedCurrentScriptID = (ModelID)ModelID.ConverterFactory.ConvertFromString(parameterText[1], inRow, inMemberMapData);
+
+            return new GameStatus(parsedPlayerCharacterID, parsedCurrentScriptID);
+        }
+        #endregion
+
+        #region Utilities
+        /// <summary>
+        /// Returns a <see cref="string"/> that represents the current <see cref="GameStatus"/>.
+        /// </summary>
+        /// <returns>The representation.</returns>
+        public override string ToString()
+            => $"[{nameof(PlayerCharacterID)} {PlayerCharacterID}, {nameof(CurrentScriptID)} {CurrentScriptID}]";
+
+        /// <summary>
+        /// Creates a new instance that is a deep copy of the current instance.
+        /// </summary>
+        /// <returns>A new instance with the same characteristics as the current instance.</returns>
+        public GameStatus DeepClone()
+            => new GameStatus(CurrentScriptID, CurrentScriptID);
+        #endregion
+    }
+}
