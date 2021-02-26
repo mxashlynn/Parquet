22462386b147a921773a0b8a0f22734cb15ec570
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-02-25 17:03:09 -0800

    Updates variable name.

--- ParquetUnitTests/Regions/RegionStatusUnitTestExtensions.cs
+++ ParquetUnitTests/Regions/RegionStatusUnitTestExtensions.cs
@@ -8,27 +8,27 @@ namespace ParquetUnitTests.Maps
     internal static class RegionStatusUnitTestExtensions
     {
         /// <summary>Fills the region with a test pattern.</summary>
-        public static RegionModel FillTestPattern(this RegionModel inMapRegionModel)
+        public static RegionModel FillTestPattern(this RegionModel inRegionModel)
         {
             for (var x = 0; x < RegionStatus.ParquetsPerRegionDimension; x++)
             {
                 for (var y = 0; y < RegionStatus.ParquetsPerRegionDimension; y++)
                 {
-                    inMapRegionModel.ParquetDefinitions[y, x].FloorID = TestModels.TestFloor.ID;
+                    inRegionModel.ParquetDefinitions[y, x].FloorID = TestModels.TestFloor.ID;
                 }
 
-                inMapRegionModel.ParquetDefinitions[0, x].BlockID = TestModels.TestBlock.ID;
-                inMapRegionModel.ParquetDefinitions[RegionModel.ParquetsPerRegionDimension, 1].BlockID = TestModels.TestBlock.ID;
+                inRegionModel.ParquetDefinitions[0, x].BlockID = TestModels.TestBlock.ID;
+                inRegionModel.ParquetDefinitions[RegionModel.ParquetsPerRegionDimension, 1].BlockID = TestModels.TestBlock.ID;
             }
             for (var y = 0; y < RegionStatus.ParquetsPerRegionDimension; y++)
             {
-                inMapRegionModel.ParquetDefinitions[y, 0].BlockID = TestModels.TestBlock.ID;
-                inMapRegionModel.ParquetDefinitions[y, RegionModel.ParquetsPerRegionDimension - 1].BlockID = TestModels.TestBlock.ID;
+                inRegionModel.ParquetDefinitions[y, 0].BlockID = TestModels.TestBlock.ID;
+                inRegionModel.ParquetDefinitions[y, RegionModel.ParquetsPerRegionDimension - 1].BlockID = TestModels.TestBlock.ID;
             }
-            inMapRegionModel.ParquetDefinitions[2, 1].FurnishingID = TestModels.TestFurnishing.ID;
-            inMapRegionModel.ParquetDefinitions[3, 3].CollectibleID = TestModels.TestCollectible.ID;
+            inRegionModel.ParquetDefinitions[2, 1].FurnishingID = TestModels.TestFurnishing.ID;
+            inRegionModel.ParquetDefinitions[3, 3].CollectibleID = TestModels.TestCollectible.ID;
 
-            return inMapRegionModel;
+            return inRegionModel;
         }
     }
 }
