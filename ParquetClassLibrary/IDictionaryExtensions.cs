db80735fe74eafcceaa9929e77884a80f400e32d
Paige Ashlynn <mx.ashlynn@gmail.com>
2021-06-23 13:59:44 -0700

    Removes unused methods.

--- ParquetClassLibrary/IDictionaryExtensions.cs
+++ ParquetClassLibrary/IDictionaryExtensions.cs
@@ -32,48 +32,11 @@ namespace Parquet
             return true;
         }
 
-        /// <summary>Clears the dictionary, then attempts to add all the given elements.</summary>
-        /// <param name="dictionaryToAlter">The <see cref="IDictionary{K, V}"/> being altered.</param>
-        /// <param name="keyValuePairs">The new content for the <see cref="IDictionary{K, V}"/>.</param>
-        /// <returns><c>true</c> if every add succeeded; <c>false</c> otherwise.</returns>
-        public static bool TryReplaceWith<TKey, TValue>(this IDictionary<TKey, TValue> dictionaryToAlter,
-                                                        IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs)
-        {
-            try
-            {
-                dictionaryToAlter.ReplaceWith(keyValuePairs);
-            }
-            catch (Exception replaceException)
-            {
-                Logger.Log(LogLevel.Error,
-                           string.Format(CultureInfo.CurrentCulture, Resources.ErrorCannotReplaceDictionary, replaceException.Message),
-                           replaceException);
-
-                return false;
-            }
-
-            return true;
-        }
-
         /// <summary>Clears the dictionary, then adds all the given elements.</summary>
         /// <param name="dictionaryToAlter">The <see cref="IDictionary{K, V}"/> being altered.</param>
         /// <param name="keyValuePairs">The new content for the <see cref="IDictionary{K, V}"/>.</param>
         /// <returns><c>true</c> if every add succeeded; <c>false</c> otherwise.</returns>
         internal static void ReplaceWith<TKey, TValue>(this IDictionary<TKey, TValue> dictionaryToAlter, IDictionary<TKey, TValue> keyValuePairs)
-        {
-            dictionaryToAlter.Clear();
-            foreach (KeyValuePair<TKey, TValue> kvp in keyValuePairs ?? new Dictionary<TKey, TValue>())
-            {
-                dictionaryToAlter[kvp.Key] = kvp.Value;
-            }
-        }
-
-        /// <summary>Clears the dictionary, then adds all the given elements.</summary>
-        /// <param name="dictionaryToAlter">The <see cref="IDictionary{K, V}"/> being altered.</param>
-        /// <param name="keyValuePairs">The new content for the <see cref="IDictionary{K, V}"/>.</param>
-        /// <returns><c>true</c> if every add succeeded; <c>false</c> otherwise.</returns>
-        internal static void ReplaceWith<TKey, TValue>(this IDictionary<TKey, TValue> dictionaryToAlter,
-                                                       IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs)
         {
             dictionaryToAlter.Clear();
             foreach (KeyValuePair<TKey, TValue> kvp in keyValuePairs ?? new Dictionary<TKey, TValue>())
